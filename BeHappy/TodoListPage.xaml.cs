9729790f9dd652b3dd7e9a268248aca5de02b407
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-13 19:56:17 +0100

    playing around and also copy files from workshop organizer[0]
    [0]: https://github.com/StargrrlMoonlight/Oslo-Xamarin-Meetup.git
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- BeHappy/TodoListPage.xaml.cs
+++ BeHappy/TodoListPage.xaml.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 
 namespace BeHappy
 {
     public partial class TodoListPage : ContentPage
     {
+        //MediaFile photo;
+
         public TodoListPage()
         {
             InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+
+            //// https://github.com/xamarin/xamarin-forms-samples/search?q=TapGestureRecognizer&unscoped_q=TapGestureRecognizer
+            //var tapGesture = new TapGestureRecognizer
+            //{
+            //    TappedCallback = async (v, o) =>
+            //    {
+            //        CameraButton_Clicked(null, null);
+            //    },
+            //    NumberOfTapsRequired = 2
+            //};
+        }
+
+        // Reused stuff from here https://xamarinhelp.com/use-camera-take-photo-xamarin-forms/
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                image.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
         }
     }
 }
