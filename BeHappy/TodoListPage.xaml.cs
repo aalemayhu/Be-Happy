3e1f7cf76804b1c93b2876f81b46863bd9ebc78c
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-13 18:20:44 +0100

    Setup project structure
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ BeHappy/TodoListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BeHappy
+{
+    public partial class TodoListPage : ContentPage
+    {
+        public TodoListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BeHappy/TodoListPaste.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace BeHappy
+{
+    internal class TodoListPaste : Page
+    {
+    }
+}
